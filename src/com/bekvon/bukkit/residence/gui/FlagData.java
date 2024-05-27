b356a0339aaf7c36819bcf3888b361c21b016ade
Zrips <zripas@gmail.com>
2024-05-27 11:45:17 +0300

    Defaulting to stone on flag icon set

--- src/com/bekvon/bukkit/residence/gui/FlagData.java
+++ src/com/bekvon/bukkit/residence/gui/FlagData.java
@@ -3,24 +3,26 @@ package com.bekvon.bukkit.residence.gui;
 import java.util.HashMap;
 import org.bukkit.inventory.ItemStack;
 
+import net.Zrips.CMILib.Items.CMIMaterial;
+
 public class FlagData {
 
     private HashMap<String, ItemStack> items = new HashMap<String, ItemStack>();
 
     public void addFlagButton(String flag, ItemStack item) {
-	this.items.put(flag, item);
+        this.items.put(flag, item == null ? CMIMaterial.STONE.newItemStack() : item);
     }
 
     public void removeFlagButton(String flag) {
-	this.items.remove(flag);
+        this.items.remove(flag);
     }
 
     public boolean contains(String flag) {
-	return items.containsKey(flag);
+        return items.containsKey(flag);
     }
 
     public ItemStack getItem(String flag) {
-	return items.get(flag);
+        return items.get(flag);
     }
 
 }
